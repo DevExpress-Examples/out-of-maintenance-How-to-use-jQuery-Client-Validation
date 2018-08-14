bdb68841b0749e0168adf5a9b444b0f872fce2eb
Roman Balaban (DevExpress) <balaban.roman@corp.devexpress.com>
2018-08-15 00:57:00 +0300

    add example contentcd

---
+++ CS/DXWebApplication/DXWebApplication/Models/JQueryValidationData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DXWebApplication {
+    public class JQueryValidationData {
+        [Display(Name = "Author:")]
+        [Required(ErrorMessage = "Author is required")]
+        public string Author { get; set; }
+
+        [Display(Name = "Publisher:")]
+        [StringLength(10, ErrorMessage = "Must be under 10 characters")]
+        public string Publisher { get; set; }
+
+        [Display(Name = "Release Date:")]
+        [Required(ErrorMessage = "Release date is required")]
+        [Remote("CheckReleaseDate", "Common", ErrorMessage = "Release date can not be earlier than today")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Annotation:")]
+        [Required(ErrorMessage = "Annotation is required")]
+        public string Annotation { get; set; }
+    }
+}
