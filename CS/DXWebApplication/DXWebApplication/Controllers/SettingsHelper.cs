b1c7c0a1d278e6094de427b9e9c3eea5d334e6cb
Roman Balaban <25555034+onebalaban@users.noreply.github.com>
2018-09-14 16:58:49 +0300

    Refactoring

--- CS/DXWebApplication/DXWebApplication/Controllers/SettingsHelper.cs
+++ CS/DXWebApplication/DXWebApplication/Controllers/SettingsHelper.cs
@@ -8,37 +8,14 @@ using System.Web;
 using System.Web.UI.WebControls;
 
 namespace DXWebApplication {
-    public class CommonDemoHelper {
-        static Action<TextBoxSettings> textBoxSettingsMethod;
+    public class SettingsHelper {
         static Action<MVCxFormLayoutItem> formLayoutItemSettingsMethod;
-        public static Action<TextBoxSettings> TextBoxSettingsMethod {
-            get {
-                if(textBoxSettingsMethod == null)
-                    textBoxSettingsMethod = CreateTextBoxSettingsMethod();
-                return textBoxSettingsMethod;
-            }
-        }
         public static Action<MVCxFormLayoutItem> FormLayoutItemSettingsMethod {
             get {
                 if(formLayoutItemSettingsMethod == null)
                     formLayoutItemSettingsMethod = CreateFormLayoutItemSettingsMethod();
                 return formLayoutItemSettingsMethod;
             }
-        }
-        public static string GetCaptionFromModel(string propertyName, Type modelType) {
-            var info = modelType.GetMember(propertyName);
-            var attributes = info[0].GetCustomAttributes(typeof(DisplayAttribute), false);
-            var caption = ((DisplayAttribute)attributes[0]).Name;
-            return caption;
-        }
-        static Action<TextBoxSettings> CreateTextBoxSettingsMethod() {
-            return settings => {
-                settings.Width = Unit.Percentage(100);
-                settings.Properties.ValidationSettings.Display = Display.Dynamic;
-                settings.Properties.ValidationSettings.ErrorTextPosition = ErrorTextPosition.Bottom;
-                settings.ShowModelErrors = true;
-                settings.Properties.ValidationSettings.ErrorDisplayMode = ErrorDisplayMode.ImageWithText;
-            };
         }
         static Action<MVCxFormLayoutItem> CreateFormLayoutItemSettingsMethod() {
             return itemSettings => {
